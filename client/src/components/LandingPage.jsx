a1638772d976a72af590d25d586254046a12144e
sai sarayu <sarayu2926@gmail.com>
2025-03-10 13:11:16 +0530

    Deployed Express.js to Cloudflare Workers

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -2,7 +2,6 @@ const LandingPage = () => {
     return (
       <div className="landing-page">
         <h1>Welcome to ASAP</h1>
-        
         <button>Get Started</button>
       </div>
     );
